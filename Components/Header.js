17c36b6f2dbeba54642aff11ef2fcdf2b99c78a4
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-03-29 13:02:03 -0300

    cria arquivo main.yml

--- Components/Header.js
+++ Components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className='header'>
       <h3>Welckson Profile</h3>
+      <section className='links-header'>
+        <Link to='/' className='links-header'>
+          <h4>Quem sou eu</h4>
+        </Link>
+        <Link to='/knowledge' className='links-header'>
+          <h4>Conhecimentos</h4>
+        </Link>
+        <Link to='/projects' className='links-header'>
+          <h4>Projetos</h4>
+        </Link>
+        <Link to='/contact' className='links-header'>
+          <h4>Contato</h4>
+        </Link>
+      </section>
     </header>
   );
 }
