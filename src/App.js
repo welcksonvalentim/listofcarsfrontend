4fdac47f7c160e10bc58ae245e031bea803b683e
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-03-29 07:50:53 -0300

    welckson valentim - build: configura app.js e index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import ProfileProvider from './Context/ProfileProvider';
+import Profile from './Pages/Profile';
+import Contact from './Pages/Contatc';
+import Knowledge from './Pages/Knowledge';
+import Projects from './Pages/Projects';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProfileProvider>
+      <Switch>
+        <Route exact path='/' component={ Profile } />
+        <Route exact path='/contact' component={ Contact } />
+        <Route exact path='/knowledge' component={ Knowledge } />
+        <Route exact path='/projects' component={ Projects } />
+      </Switch>
+    </ProfileProvider>
   );
 }
 
