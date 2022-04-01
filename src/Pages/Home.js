aeba77cb9c96507902b144789202215a85979534
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-01 09:18:04 -0300

    welckson valentim - feat: adiciona texto com informaçoe mais detalhadas da aplicação

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,8 +6,8 @@ function Home() {
       <Header />
       <section className='home'>
         <h3>Descrição Aplicação</h3>
-        <p>A aplicação possibilita interação com o banco de dados, permitindo: </p>
-        <p>Exibição de Marcas, modelos e versões de veículos</p>
+        <p>A aplicação possibilita interação com o banco de dados, permitindo: Exibição de Marcas, modelos e versões de veículos.</p>
+        <p>Ao clicar sobre o card será redirecionado para página especializada, com informações técnicas do veículo.</p>
       </section>
     </ main>
   );
